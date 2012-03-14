1697b01cb752d64d20ae85b657d599c368f69ff5
Rune Andersen <rune@putty.dk>
2012-03-14 21:52:03 +0100

    added more files

---
+++ distance.php
@@ -0,0 +1,36 @@
+<?php
+/*
+
+$lat1 = 55.66157579999999;
+$lon1 = 12.4049744;
+*/
+
+$lat1 = $_POST['lat'];
+$lon1 = $_POST['lon'];  
+
+require('gPoint.php');
+
+include('stations.inc');
+
+$stations = array();
+
+foreach($stationlist as $uic => $stationinfo) {
+  $utm_parts = explode(';', $stationinfo['utm']);
+  $station = new gPoint();
+  $station->setUTM($utm_parts[0], $utm_parts[1], '32N');
+  $station->convertTMtoLL();
+ 
+  $stations[] = array('uic' => $uic - 8600000, 'name' => utf8_decode($stationinfo['station']), 'distance' => $station->distanceFrom($lon1, $lat1));
+}
+
+usort($stations, 'distancesort');
+
+print(json_encode($stations));
+
+
+function distancesort($a, $b) {
+  if ($a['distance'] == $b['distance']) {
+    return 0;
+  }
+  return ($a['distance'] < $b['distance']) ? -1 : 1;
+}
---
+++ stogdata.php
@@ -0,0 +1,43 @@
+<?php
+$uic = $_POST['uic'];
+
+$url = 'http://wap.dsb.dk/servlet/WapServlet?stationsid='.$uic;
+
+$file = file_get_contents($url);
+
+
+$seperated = str_replace('<br/>', ';', $file);
+$stripped = strip_tags($seperated);
+$list = array_filter(explode(';', $stripped));
+
+$output = array();
+
+$count = 0;
+
+foreach ($list as $entry){
+	$count++;
+
+	if($count != count($list)){
+		if (!preg_match('/\d|½/', $entry)){
+      if(strpos($entry, 'Tilbage') === FALSE) {
+        $output[] = array('head' => $entry);
+    
+      }
+		
+		}
+		else {
+			if (preg_match('/^([\w]+) (.*) ([\d]+|\.)$/', $entry, $matches)) {
+        $station_info = array_splice($matches, 1);
+        $output[] = array(
+          'line' => $station_info[0],
+          'destination' => utf8_encode($station_info[1]),
+          'departure' => $station_info[2],
+        );
+			}
+		}
+	}
+}
+
+print json_encode($output);
+
+?>
